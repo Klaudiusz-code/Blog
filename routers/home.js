a0ac29bc6f89ccbd39377c7c6b450216b3307337
Klaudiusz <muminowy123@gmail.com>
2023-03-06 10:08:17 +0100

    MAin router added

---
+++ routers/home.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const multer = require('multer');
+
+const routers = require('routers')
+const {db} = require("../utils");
+
+const app = express();
+const upload = multer({ storage: this.storage });
+
+const homeRouter = express.Router();
+
+
+
+homeRouter
+    .get('/',(req,res) => {
+        res.render('blogs/list-blogs',{
+            blogs: db.getAll(),
+        })
+    })
+    .get('/:id',(req, res) =>{
+        res.render('blogs/view-blog',{
+            blog: db.getOne(req.params.id),
+        })
+    })
+    .post('/',(req,res) =>{
+        const id = db.create(req.body);
+
+         res.render('blogs/added',{
+             title: req.body.title,
+             id: id
+         })
+    })
+    .put('/:id', (req,res) => {
+        res.send('zmodyfikuj')
+    })
+    .delete('/:id',(req,res) =>{
+        db.delete(req.params.id);
+        res.render('blogs/deleted')
+    })
+    .get('/form/add', (req,res) => {
+        res.render('blogs/forms/add')
+    })
+    .get('/form/edit/:id', (req,res) => {
+        res.render('blogs/forms/edit',{
+            blog: db.getOne(req.params.id)
+        })
+    })
+
+module.exports = {
+    homeRouter,
+}
