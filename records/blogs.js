3bc999989a0d0dae4284455c34582d33f00eecab
Klaudiusz <muminowy123@gmail.com>
2023-03-08 08:19:05 +0100

    Added validation for img

--- records/blogs.js
+++ records/blogs.js
@@ -2,12 +2,7 @@ const {ValidationError} = require("../utils/error");
 
 class Blogs {
     constructor(obj) {
-        const {id,name,title,longDesc} = obj;
-
-        if (!id || typeof id !== 'string'){
-            throw new ValidationError('Id musi być tekstem')
-        }
-
+        const {id,name,title,longDesc,img} = obj;
         if (!name || typeof name !== 'string' || name.length < 3){
             throw new ValidationError('Imię musi być tekstem o długości minimum 3 znaków')
         }
@@ -17,11 +12,14 @@ class Blogs {
         if (!longDesc){
             throw new ValidationError('Wypełnij tytuł')
         }
-
+       if (!img){
+           throw new ValidationError('Dodaj obrazek')
+       }
         this.id = id;
         this.name = name;
         this.title = title;
-        this.longDesc = longDesc;
+        this.longDesc = longDesc
+        this.img = img;
     }
 }
 
